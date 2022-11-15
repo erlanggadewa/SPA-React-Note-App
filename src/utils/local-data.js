bb4568587e5713e1c02e34382b659f2a07edc0d0
erlanggadewa <erlanggadewa7@gmail.com>
2022-11-15 21:43:29 +0700

    fix local data archive status

--- src/utils/local-data.js
+++ src/utils/local-data.js
@@ -5,7 +5,7 @@ let notes = [
     body: "Babel merupakan tools open-source yang digunakan untuk mengubah sintaks ECMAScript 2015+ menjadi sintaks yang didukung oleh JavaScript engine versi lama. Babel sering dipakai ketika kita menggunakan sintaks terbaru termasuk sintaks JSX.",
     createdAt: "2022-04-14T04:27:34.572Z",
     imgUrl: "https://source.unsplash.com/random",
-    archived: true,
+    archived: false,
   },
   {
     id: "notes-2",
